e14e2ddd49afb82c1ddfdb80d2e86d77e19a7985
j-guzik <justyna.b.guzik@gmail.com>
2021-03-12 19:08:46 +0100

    main screen logic

---
+++ scripts/MainMenu.esm.js
@@ -0,0 +1,43 @@
+import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from './Common.esm.js';
+import { levelSelect } from './LevelSelect.esm.js';
+
+const START_SCREEN_ID = 'js-start-screen';
+const START_SCREEN_GAME_BUTTON_ID = 'js-start-game';
+const START_SCREEN_SETTINGS_BUTTON_ID = 'js-settings-button';
+const SCALE_PROPERTY = '--scale-value';
+
+
+class MainMenu extends Common {
+    constructor(){
+        super(START_SCREEN_ID);
+        this.bindToGameElements();
+        this.resizeGameWindow();
+        window.addEventListener('resize', this.resizeGameWindow);
+    }
+
+    bindToGameElements(){
+        const gameStartButton = this.bindToElement(START_SCREEN_GAME_BUTTON_ID);
+        const gameSettingsButton = this.bindToElement(START_SCREEN_SETTINGS_BUTTON_ID);
+
+        gameStartButton.addEventListener('click', () => this.showLevelScreen());
+        gameSettingsButton.addEventListener('click', () => this.showSettingsScreen());
+    }
+
+    showLevelScreen(){
+        this.changeVisibilityScreen(this.element, HIDDEN_SCREEN);
+        this.changeVisibilityScreen(levelSelect.element, VISIBLE_SCREEN);
+    }
+
+    showSettingsScreen(){
+        console.log('Ustawienia gry');
+    }
+
+    resizeGameWindow() {
+        const { innerWidth: width, innerHeight: height } = window;
+        const scale = Math.min(width / 880, height / 650);
+
+        document.documentElement.style.setProperty(SCALE_PROPERTY, scale);
+    }
+}
+
+export const mainMenu = new MainMenu();
