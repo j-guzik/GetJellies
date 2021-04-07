ff40cd6b9688f938a8419b90e67d9a7b0a6495b1
j-guzik <justyna.b.guzik@gmail.com>
2021-04-07 14:35:26 +0200

    speed change

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -13,9 +13,9 @@ import { mainMenu } from './MainMenu.esm.js';
 export const DIAMOND_ARRAY_WIDTH = 8;
 
 const DIAMOND_ARRAY_HEIGHT = DIAMOND_ARRAY_WIDTH + 1;
-const SWAPING_SPEED = 9;
+const SWAPING_SPEED = 8;
 const LAST_ELEMENT_DIAMONDS_ARRAY = DIAMOND_ARRAY_WIDTH * DIAMOND_ARRAY_HEIGHT - 1;
-const TRANSPARENCY_SPEED = 14;
+const TRANSPARENCY_SPEED = 11;
 
 class Game extends Common {
     constructor() {
